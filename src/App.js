67bb71eb80c9fa934f04aa9191986211353e61e6
Vishnuoffl72 <vishnuoffl72@gmail.com>
2024-02-04 11:24:49 +0530

    made changes for hosting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className='base'>
       {/* <Loginpage /> */}
       <BrowserRouter>
-        <Routes>
+        <Routes basename="/Truckers-app">
           <Route path='/' element={<Homepage />}/>
           <Route path='/Postjob' element={< Postpage />}/>
         </Routes>
